85e856bd733bfb2c1b05747d959d224f40a9149c
NinkingGaming <NinkingGaming@users.noreply.github.com>
2020-08-07 00:46:46 -0500

    h

---
+++ script/math.js
@@ -0,0 +1,21 @@
+var loadChance1 = Math.floor(Math.random() * 19);
+
+var loadChance2 = Math.floor(Math.random() * 51);
+
+var loadChanceComplete = loadChance1 + loadChance2;
+
+console.log( loadChance1)
+
+console.log( loadChance2)
+
+console.log( 'Code Generated: ', loadChanceComplete )
+
+if ( loadChance1 + loadChance2 == 16) {
+    window.open("/pages/youwon.html" , "_blank");
+    console.log("You won!!")
+}
+
+if ( loadChance1 + loadChance2 == 21) {
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ" , "_blank");
+    console.log("Fuck You LOL")
+}
